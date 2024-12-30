93523a03c121217d3fd37962699267b221eb8366
kshitij <kshitij@kshitij.myguest.virtualbox.org>
2024-12-30 11:02:15 +0800

    Adding error handling in routes.py

--- app/routes.py
+++ app/routes.py
@@ -10,28 +10,26 @@ def home():
 @app.route("/earnings_transcript_summary", methods=["POST"])
 def summarize():
     data = request.get_json()
-    # print("data: ",data)
+
+    # Check for missing fields
+    if "transcript_text" not in data:       
+      return jsonify({"error": "No transcript_text content provided"}), 400
+    if "company_name" not in data:
+      return jsonify({"error": "No company_name provided"}), 400
+
     content = data.get("transcript_text", "")
-    # print("content: ",content)
     company_name=data.get("company_name", "")
-    # print("company_name: ",company_name)
+
+    # Check for empty data
     if not content:
-        return jsonify({"error": "No content provided"}), 400
+        return jsonify({"error": "No transcript_text content provided"}), 400
+    if not company_name:
+        return jsonify({"error": "No company_name provided"}), 400
 
     summary_financial_performance = summarize_text(content,"financial performance",company_name)
-    print("summary_financial_performance: ",summary_financial_performance)
-    print("#################################################")
     summary_market_dynamics = summarize_text(content,"market dynamics",company_name)
-    print("summary_market_dynamics: ",summary_market_dynamics)
-    print("#################################################")
     summary_expansion_plans = summarize_text(content,"expansion plans",company_name)
-    print("summary_expansion_plans: ",summary_expansion_plans)
-    print("#################################################")
     summary_environmental_risks = summarize_text(content,"environmental_risks",company_name)
-    print("summary_environmental_risks: ",summary_environmental_risks)
-    print("#################################################")
     summary_regulatory_or_policy_changes = summarize_text(content,"regulatory or policy changes",company_name)
-    print("summary_regulatory_or_policy_changes: ",summary_regulatory_or_policy_changes)
-    print("#################################################")
-    
+
     return jsonify({"company_name": company_name, "financial_performance": summary_financial_performance, "market_dynamics": summary_market_dynamics, "expansion_plans": summary_expansion_plans, "environmental_risks": summary_environmental_risks, "regulatory_or_policy_changes": summary_regulatory_or_policy_changes})
